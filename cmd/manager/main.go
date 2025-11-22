2ca763b3b1f54b42a35af5cd25e722d2de3ebf32
Daniil Leonov <yshmgrt@gmail.com>
2025-11-22 20:26:11 +0300

    feat: load .env file

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -6,15 +6,23 @@ import (
 	"log/slog"
 
 	"github.com/LeonovDS/review-manager/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Warn("Unable to load .env file, using system environment", slog.Any("err", err))
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	_, err := database.Connect(ctx)
+	_, err = database.Connect(ctx)
 	if err != nil {
-		slog.Error("Unable to perform migrations", "err", err)
+		slog.Error("Unable to perform migrations", slog.Any("err", err))
 		return
 	}
+
+	slog.Info("DB connection created")
 }
