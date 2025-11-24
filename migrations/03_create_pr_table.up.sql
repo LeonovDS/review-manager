f2b52901b896f043b87a1bbc606c71c3f29c1b84
Daniil Leonov <yshmgrt@gmail.com>
2025-11-24 16:21:05 +0300

    feat: add pr table

---
+++ migrations/03_create_pr_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TYPE Status AS ENUM ('OPEN', 'MERGED');
+
+CREATE TABLE IF NOT EXISTS PullRequest (
+    pull_request_id SERIAL PRIMARY KEY,
+    pull_request_name TEXT NOT NULL,
+    author_id TEXT NOT NULL REFERENCES Users(user_id),
+    status Status NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    merged_at TIMESTAMP
+);
